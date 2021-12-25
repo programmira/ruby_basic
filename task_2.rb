861e86fdd7a196731fd3e57cfd61e2136994cdca
Ira <irochka.ukhova@gmail.com>
2021-12-25 18:06:32 +0200

    corrected ex except ex.4

--- task_2.rb
+++ task_2.rb
@@ -1,6 +1,6 @@
 puts "Введите сторону треугольника"
-a = gets.chomp.to_i
+a = gets.chomp.to_f
 puts "Введите высоту треугольника"
-h = gets.chomp.to_i
+h = gets.chomp.to_f
 square = 0.5 * a * h
 puts "Площадь треугольника равна #{square}"
